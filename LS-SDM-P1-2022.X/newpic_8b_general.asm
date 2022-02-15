74622c21a657da02d565c55cc80d0c95d4efce72
Albert Garangou Culebras <albertgarangou@gmail.com>
2022-02-15 10:17:51 +0100

    Bye bye eeprom
    gg wp

--- LS-SDM-P1-2022.X/newpic_8b_general.asm
+++ LS-SDM-P1-2022.X/newpic_8b_general.asm
@@ -6,9 +6,7 @@ LIST P=PIC18F4321	F=INHX32
     CONFIG WDT=OFF	    ;Desactivem el Watch Dog Timer
     CONFIG LVP=OFF	    ;Evitar resets eusart
 ;--------------------------------DECLARACIO_VARS--------------------------------
- eeprom_adr EQU 0x00
- eeprom_data EQU 0x01
- carrier EQU 0x02
+ carrier EQU 0x01
  
 ;-------------------------------------INITS-------------------------------------
     ORG 0x000
@@ -47,11 +45,6 @@ CARREGA_TIMER
     MOVLW LOW(.)
     MOVWF TMR0L,0
 RETURN
-
-INIT_EEPROM
-    BCF EECON1, EEPGD
-    BCF EECON1, CFGS
-RETURN
     
 INIT_ADCON
     
@@ -88,36 +81,3 @@ HIGH_RSI
 RETFIE FAST
     
 ;-----------------------------------FUNCIONS------------------------------------
-
-;Lectura EEPROM
-EEPROM_READ
-    movf eeprom_addr,0
-    movwf EEADR,0
-    BCF EECON1, EEPGD    ; Point to DATA memory
-    BCF EECON1, CFGS     ; Access EEPROM
-    BSF EECON1, RD        ; EEPROM Read
-    MOVFF EEDATA, eeprom_data
-RETURN
-    
-;Escriptura EEPROM   ;moure el que sigui a "eeprom_addr" i "eeprom_data"
-EEPROM_WRITE
-    movf eeprom_addr,0
-    movwf EEADR,0
-    movf eeprom_data,0
-    movwf EEDATA,0
-    bsf EECON1,WREN
-    bcf INTCON,GIE
-    movlw 55h
-    movwf EECON2,0
-    movlw 0AAh
-    movwf EECON2
-    bsf EECON1,WR
-    call ESPERA_EEPROM_ESCRIURE
-    bsf INTCON,GIE
-    bcf EECON1,WREN
-    return
-    
-ESPERA_EEPROM_ESCRIURE
-    BTFSC EECON1,WR
-    GOTO ESPERA_EEPROM_ESCRIURE
-    RETURN
