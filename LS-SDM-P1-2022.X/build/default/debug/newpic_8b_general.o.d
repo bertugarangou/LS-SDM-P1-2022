e0695f6719922a87cb7242859bd1214d7bc7c716
gerythebest <gerard.massana@students.salle.url.edu>
2022-02-22 22:38:51 +0100

    Token change manual mode

--- LS-SDM-P1-2022.X/build/default/debug/newpic_8b_general.o.d
+++ LS-SDM-P1-2022.X/build/default/debug/newpic_8b_general.o.d
@@ -1,3 +1,3 @@
 build/default/debug/newpic_8b_general.o:  \
-	C:/Users/alber/Documents/GITHUB/LS-SDM-P1-2022/LS-SDM-P1-2022.X/newpic_8b_general.asm  \
+	C:/Users/gerar/Documents/Uni/3r\ Telecos/Compus\ P2_FaseA/LS-SDM-P1-2022/LS-SDM-P1-2022.X/newpic_8b_general.asm  \
 
