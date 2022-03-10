65f8ac8e1ef52c6a828e4283f8fe6d8b4d056516
gerythebest <gerard.massana@students.salle.url.edu>
2022-03-10 11:15:51 +0100

    gg wp

--- LS-SDM-P1-2022.X/build/default/production/newpic_8b_general.o.d
+++ LS-SDM-P1-2022.X/build/default/production/newpic_8b_general.o.d
@@ -1,3 +1,3 @@
 build/default/production/newpic_8b_general.o:  \
-	C:/Users/gerar/OneDrive/Documents/GitHub/LS-SDM-P1-2022/LS-SDM-P1-2022.X/newpic_8b_general.asm  \
+	C:/Users/gerar/Documents/Uni/3r\ Telecos/Compus\ P2_FaseA/LS-SDM-P1-2022/LS-SDM-P1-2022.X/newpic_8b_general.asm  \
 
